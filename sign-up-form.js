1c789a14a21b591722d70c2b489a2e30ea016c3c
sophiavf <15851587+sophiavf@users.noreply.github.com>
2023-02-27 16:37:38 +0100

    implemented form validation logic preventing the user from submitting if inputs are invalid

--- sign-up-form.js
+++ sign-up-form.js
@@ -1,7 +1,12 @@
-var passwordInputs = document.getElementsByClassName("error");
-var pw1 = document.getElementById("user_password");
-var pw2 = document.getElementById("confirm_password");
+const passwordInputs = document.getElementsByClassName("error");
+const pw1 = document.getElementById("user_password");
+const pw2 = document.getElementById("confirm_password");
 const error = pw1.nextElementSibling;
+const allInputs = document
+	.getElementById("form")
+	.querySelectorAll("[required]");
+
+var passwordValidity = false;
 
 function matchPassword() {
 	// if the confirm?password input is onfocus, this function is run to check if the passwords match
@@ -9,19 +14,20 @@ function matchPassword() {
 		error.className = "errorMessage active";
 		passwordInputs.className = "error invalid";
 		error.textContent = "*Passwords do not match";
+		passwordValidity = false;
 	} else {
 		//if the passwords match, the error message is removed
 		error.textContent = "";
 		error.className = "error";
 		passwordInputs.className = "error";
-		return;
+		passwordValidity = true;
 	}
 }
 
 const submitButton = document.querySelector('button[type="submit"]');
 const popupWindow = document.querySelector(".popup");
 const closeButton = document.querySelector(".close");
-const formElement = document.querySelector("form")
+const formElement = document.querySelector("form");
 
 submitButton.addEventListener("click", () => {
 	popupWindow.style.display = "block";
@@ -29,6 +35,23 @@ submitButton.addEventListener("click", () => {
 
 closeButton.addEventListener("click", () => {
 	popupWindow.style.display = "none";
-	formElement.reset(); 
+	formElement.reset();
 });
 
+allInputs.forEach((input) => {
+	input.addEventListener("input", () => {
+		updateSubmitButton();
+	});
+});
+
+function updateSubmitButton() {
+	const firstName = document.getElementById("first_name").validity.valid;
+	const lastName = document.getElementById("last_name").validity.valid;
+	const email = document.getElementById("user_email").validity.valid;
+
+	if (firstName && lastName && email && passwordValidity) {
+		submitButton.removeAttribute("disabled");
+	} else {
+		submitButton.setAttribute("disabled", "disabled");
+	}
+}
