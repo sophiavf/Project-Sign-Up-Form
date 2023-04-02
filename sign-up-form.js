fbd4127878ad89591ac7445726ae8273ae3e120b
sophia <15851587+sophiavf@users.noreply.github.com>
2023-04-02 11:07:51 +0200

    - Fixed form validation using 'The Constraint Validation API' - Improved form button styling

--- sign-up-form.js
+++ sign-up-form.js
@@ -6,52 +6,34 @@ const allInputs = document
 	.getElementById("form")
 	.querySelectorAll("[required]");
 
+const form = document.getElementById("form");
+const popupWindow = document.querySelector(".popup");
+const closeButton = document.querySelector(".close");
+const formElement = document.querySelector("form");
+
 var passwordValidity = false;
 
 function matchPassword() {
 	// if the confirm?password input is onfocus, this function is run to check if the passwords match
-	if (pw1.value != pw2.value) {
+	if (pw1.value !== pw2.value || pw1.value === undefined) {
 		error.className = "errorMessage active";
 		passwordInputs.className = "error invalid";
 		error.textContent = "*Passwords do not match";
-		passwordValidity = false;
+		pw1.setCustomValidity("*Passwords do not match");
+		return false;
 	} else {
 		//if the passwords match, the error message is removed
 		error.textContent = "";
 		error.className = "error";
 		passwordInputs.className = "error";
-		passwordValidity = true;
+		pw1.setCustomValidity("");
+		return true;
 	}
 }
-
-const submitButton = document.querySelector('button[type="submit"]');
-const popupWindow = document.querySelector(".popup");
-const closeButton = document.querySelector(".close");
-const formElement = document.querySelector("form");
-
-submitButton.addEventListener("click", () => {
+form.addEventListener("submit", function () {
 	popupWindow.style.display = "block";
 });
-
 closeButton.addEventListener("click", () => {
 	popupWindow.style.display = "none";
 	formElement.reset();
 });
-
-allInputs.forEach((input) => {
-	input.addEventListener("input", () => {
-		updateSubmitButton();
-	});
-});
-
-function updateSubmitButton() {
-	const firstName = document.getElementById("first_name").validity.valid;
-	const lastName = document.getElementById("last_name").validity.valid;
-	const email = document.getElementById("user_email").validity.valid;
-
-	if (firstName && lastName && email && passwordValidity) {
-		submitButton.removeAttribute("disabled");
-	} else {
-		submitButton.setAttribute("disabled", "disabled");
-	}
-}
